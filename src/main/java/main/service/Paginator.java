208de140218253e06db3e186dd73e2652f6b9ea5
IvanShport <shportie@student.bmstu.ru>
2018-04-05 16:47:49 +0300

    paginator

--- src/main/java/main/service/Paginator.java
+++ src/main/java/main/service/Paginator.java
@@ -6,16 +6,32 @@ public class Paginator<T> {
 
     public List<T> paginate(Integer page, List<T> array) {
 
-        Integer numOfPages = array.size() / 10 + 1;
+        Integer numOfPages = array.size() / 10;
+        Integer remainder = array.size() % 10;
+
+        if (remainder != 0) {
+            numOfPages++;
+        }
+
         if (page < 1) {
             page = 1;
+        }
+
+        if (numOfPages == 0) {
+            numOfPages = 1;
         }
 
         if (page > numOfPages) {
             page = numOfPages;
         }
 
-        return array.subList((page - 1) * 10, page * 10);
+
+        if (page*10 >= array.size()) {
+            return array.subList((page - 1) * 10, (array.size() > 0) ? array.size() - 1 : 0);
+        }
+        else {
+            return array.subList((page - 1) * 10, page * 10);
+        }
 
     }
 }
