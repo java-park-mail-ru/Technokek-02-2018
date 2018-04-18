1209319ff1d0949a9195f85f0392689abeb5e531
Vlad Busov <to2002to2002@gmail.com>
2018-04-18 20:57:30 +0300

    delette imports

--- src/main/java/main/service/avatars/AvatarStorageService.java
+++ src/main/java/main/service/avatars/AvatarStorageService.java
@@ -3,10 +3,7 @@ package main.service.avatars;
 import main.domain.User;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.nio.file.Path;
-import java.util.stream.Stream;
-
 
 public interface AvatarStorageService {
 
